482fd24658bb1d359593157104aaf7031091cdee
corbinBG <corbin@boldergames.com>
2020-05-30 15:23:52 -0700

    added objects to reveal bug

---
+++ Assets/SwitchScenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour {
+  public string sceneName;
+
+  void OnTriggerEnter(Collider col) {
+    if (col.GetComponent<WebXR.WebXRController>()) {
+      SceneManager.LoadScene(sceneName);
+    }
+  }
+}
